648df2db1b5b75f65ac93ec518a2b2d846e877f1
zhangyanyu <zyy_id@163.com>
2019-04-10 14:29:17 +0800

    file_wr

--- file_wr/file_wr/open.cpp
+++ file_wr/file_wr/open.cpp
@@ -3,23 +3,21 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-void error_handing(char *message) {
-	fputs(message, stderr);
-	fputc('\n', stderr);
-	exit(1);
-}
-
 int main(int argc, char *argv[]) {
 	int fd;
 	char buf[] = "let us go tcp";
 	fd = open("data.txt", O_CREAT | O_WRONLY | O_TRUNC);
 	if (fd == -1)
 	{
-		error_handing("open() error");
+		printf("open() error\n");
+		return -1;
 	}
 	printf("file desciptor :%d\n", fd);
 	if (write(fd, buf, sizeof(buf)) == -1)
-		error_handing("write() error");
+	{
+		printf("write() error\n");
+		return -1;
+	}
 	close(fd);
 	return 0;
 }
--- file_wr/file_wr/read.cpp
+++ file_wr/file_wr/read.cpp
@@ -3,11 +3,6 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-void error_handing(char *message) {
-	fputs(message, stderr);
-	fputc('\n', stderr);
-	exit(1);
-}
 
 int main(int argc, char *argv[]) {
 	int fd;
@@ -15,12 +10,16 @@ int main(int argc, char *argv[]) {
 	fd = open("data.txt", O_RDONLY);
 	if (fd == -1)
 	{
-		error_handing("open() error");
+		printf("open() error\n");
+		return -1;
 	}
 	printf("file desciptor :%d\n", fd);
 	if (read(fd, buf, sizeof(buf)) == -1)
-		error_handing("read() error");
-	printf("read file message: %s", buf);
+	{
+		printf("read() error\n");
+		return -1;
+	}
+	printf("read file message: %s\n", buf);
 	close(fd);
 	return 0;
 }
